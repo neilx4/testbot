9db380408f74a348aa248f0c62d2376736f8045a
neilx4 <42234068+neilx4@users.noreply.github.com>
2018-10-10 10:49:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ def send_message(recipient_id, message_text):
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+        print(r.status_code)
+        print(r.text)
 
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
